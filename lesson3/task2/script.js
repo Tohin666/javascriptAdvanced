cb056e33e3cfafbab0ea7354263057b9616ca0a2
Tohin666 <tohin666@gmail.com>
2018-06-08 13:00:50 +0300

    сделал task2

--- lesson3/task2/script.js
+++ lesson3/task2/script.js
@@ -1,5 +1,7 @@
 var sourceText = document.getElementById('source-text').textContent;
-var replacedText = sourceText.replace(/'/g, '"');
+var replacedText = sourceText.replace(/^'/g, '"');
+replacedText = replacedText.replace(/\s'/g, ' "');
+replacedText = replacedText.replace(/'\s/g, '" ');
 var newParagraph = document.createElement('p');
 newParagraph.textContent = replacedText;
 document.body.appendChild(newParagraph);
