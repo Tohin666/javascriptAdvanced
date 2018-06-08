9db23f8635e7d0c55c526c77ad501414e7c34686
Tohin666 <tohin666@gmail.com>
2018-06-08 20:19:50 +0300

    сделал task3

--- lesson3/task3/script.js
+++ lesson3/task3/script.js
@@ -1,21 +1,50 @@
 var button = document.getElementById('button');
 button.onclick = function () {
+  var message = '';
 
-  while (true) {
-    var name = document.getElementById('name');
-    var nameValue = name.value;
-    var phone = document.getElementById('phone');
-    var phoneValue = phone.value;
-    if (/[^a-zа-яё]/i.test(nameValue)) {
-      name.style.borderColor = 'red';
-      alert('Имя может содержать только буквы латинского или русского алфавита!')
 
-    } else if (/[0-9]/.test(phoneValue)) {
+  var name = document.getElementById('name');
+  var nameValue = name.value;
 
-      name.style.borderColor = 'gray';
-      phone.style.borderColor = 'red';
-      alert('Введите телефон в формате +7(000)000-0000')
-    }
-    break
+  if (/[^a-zа-яё]/i.test(nameValue) || nameValue === '') {
+    name.style.borderColor = 'red';
+    message = 'Имя может содержать только буквы латинского или русского алфавита!';
+
+  } else {
+    name.style.borderColor = 'inherit';
+  }
+
+
+  var phone = document.getElementById('phone');
+  var phoneValue = phone.value;
+  if (!(/\+7\(\d{3}\)\d{3}-\d{4}/.test(phoneValue)) || phoneValue === '') {
+    phone.style.borderColor = 'red';
+    message += '\nВведите телефон в формате +7(000)000-0000';
+  } else {
+    phone.style.borderColor = 'inherit';
+  }
+
+  var mail = document.getElementById('mail');
+  var mailValue = mail.value;
+  if (!(/\w+@\w+\.\w+/.test(mailValue)) || mailValue === '') {
+    mail.style.borderColor = 'red';
+    message += '\nВведите правильный e-mail';
+  } else {
+    mail.style.borderColor = 'inherit';
+  }
+
+  var text = document.getElementById('text');
+  var textValue = text.value;
+  if (!(/[\wа-яё]/i.test(textValue)) || textValue === '') {
+    text.style.borderColor = 'red';
+    message += '\nВведите текст сообщения!';
+  } else {
+    text.style.borderColor = 'inherit';
+  }
+
+  if (message !== '') {
+    alert(message)
+  } else {
+    alert('Данные успешно отправлены!')
   }
 };
