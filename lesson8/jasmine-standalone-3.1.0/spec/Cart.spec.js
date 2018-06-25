32759e7fd589626dabd4681141ea170e53fac22e
Tohin666 <tohin666@gmail.com>
2018-06-25 15:03:44 +0300

    начал lesson8

--- lesson8/jasmine-standalone-3.1.0/spec/Cart.spec.js
+++ lesson8/jasmine-standalone-3.1.0/spec/Cart.spec.js
@@ -1,3 +1,15 @@
-describe('Модуль корзина', function () {
-  it
-})
+describe('Module: Cart', function () {
+  it('should return total sum', function () {
+    var cart = new Cart([
+      {
+        quantity: 2,
+        price: 4
+      },
+      {
+        quantity: 1,
+        price: 6
+      }
+    ]);
+    expect(cart.total()).toBe(14)
+  })
+});
